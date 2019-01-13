6eede10bbc8bc13793c263c2942cf14eb24887dc
drutter89 <dylanmrutter89@gmail.com>
2019-01-13 02:50:36 +0000

    function that rolls a loaded die in order so we know whats coming

---
+++ makeLoadedDie.js
@@ -0,0 +1,32 @@
+function makeLoadedDie() {
+    var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+    /* your code here */
+    // return Math.floor(1 + Math.random() * 6);
+    var currentRoll = 0;
+ 
+
+  
+    return function() {
+      /* your code here */
+        currentRoll++
+        return list[currentRoll -1]
+    
+      // console.log(currentRoll);
+    }
+      console.log(list[i]);
+    }
+  
+  
+  var rollLoadedDie = makeLoadedDie();
+  
+  console.log(rollLoadedDie());  // 5
+  console.log(rollLoadedDie());  // 4
+  console.log(rollLoadedDie());  // 6
+   console.log(rollLoadedDie());
+    console.log(rollLoadedDie());
+     console.log(rollLoadedDie());
+     console.log(rollLoadedDie());
+     console.log(rollLoadedDie());
+     console.log(rollLoadedDie());
+     console.log(rollLoadedDie());
+  
